56c86d24f9249efb3359667960386a0e5b1d9870
Sabih <sabih.ulhasan@yahoo.com>
2025-02-26 09:54:02 +0100

    Initial commit

---
+++ KidSketch/KidSketchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidSketchApp.swift
+//  KidSketch
+//
+//  Created by Muhammad Sabihul Hasan on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct KidSketchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
